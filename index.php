b7e385432a6c303963e76fe3203d884044e1aebc
63070047 <73648780+63070047@users.noreply.github.com>
2020-10-30 12:48:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<?php
-echo "<h2>PHP is fun!</h2>";
-echo "Hello world<br>";
-?>
+<html>
+    <head>
+        <title>Hello world</title>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <h1>My First Heading</h1>
+        <p>My First Paragraph</p>
+        <img src="cat.jpg">
+        <img src="cat2.jpg">
+        <br>
+        <a href="http//www.it.kmitl.ac.th">This is a Link</a>
+    </body>
+</html>
